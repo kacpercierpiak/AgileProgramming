0c0c69c6b37ac576cce9e2077d569f79a73dad18
KACPER CIERPIAK <KACPER_CIERPIAK@jabil.com>
2021-05-13 22:02:31 +0200

    fix ssl

--- ProjectManager/ClientApp/src/environments/environment.prod.ts
+++ ProjectManager/ClientApp/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   name: 'local',
   urls: {
-    app: 'https://agileprogramming.herokuapp.com/',
-    api: 'https://agileprogramming.herokuapp.com/api/'
+    app: 'http://agileprogramming.herokuapp.com/',
+    api: 'http://agileprogramming.herokuapp.com/api/'
   }
 };
