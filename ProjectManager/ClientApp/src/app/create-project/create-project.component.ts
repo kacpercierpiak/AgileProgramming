07ffd637aba40b5c3c787386537e06e2d06092c6
KACPER CIERPIAK <KACPER_CIERPIAK@jabil.com>
2021-05-15 14:17:46 +0200

    Fix

--- ProjectManager/ClientApp/src/app/create-project/create-project.component.ts
+++ ProjectManager/ClientApp/src/app/create-project/create-project.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 import { ProjectService } from './project.service';
 import { stringify } from '@angular/compiler/src/util';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-create-project',
   templateUrl: './create-project.component.html',
@@ -15,10 +16,10 @@ export class CreateProjectComponent implements OnInit {
   public deadline: string = "";
   public error: string = "";
 
-  constructor(private projectService: ProjectService) { }
+  constructor(private projectService: ProjectService,private router: Router) { }
 
   ngOnInit(): void {
-    this.createDateTime = new Date().toLocaleDateString();
+    this.createDateTime = new Date().toISOString();
   }
 
   public onSubmit() {
@@ -26,8 +27,13 @@ export class CreateProjectComponent implements OnInit {
     if (this.projectName == "" || this.description == "" || this.createDateTime == "" || this.deadline == "") {
       alert("Fill the fields")
     } else {
+      console.log(this.createDateTime);
+      console.log(this.deadline);
+      console.log(new Date(this.deadline));
+      console.log(new Date(this.createDateTime));
       this.projectService.create(this.projectName, this.description, new Date(this.createDateTime), new Date(this.deadline), true).subscribe(() => {
         alert("Project added");
+        this.router.navigate(["/projects"]);
       }, (error: HttpErrorResponse) => {
         if (error && error.error) {
           this.error = error.error;
