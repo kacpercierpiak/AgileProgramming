1f7c1b9fb5769265e3cb926e74e57e7445300ff0
kacpercierpiak <61351479+kacpercierpiak@users.noreply.github.com>
2021-07-19 21:36:39 +0200

    Update environment.prod.ts

--- ProjectManager/ClientApp/src/environments/environment.prod.ts
+++ ProjectManager/ClientApp/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   name: 'local',
   urls: {
-    app: 'http://agileprogramming.herokuapp.com/',
-    api: 'http://agileprogramming.herokuapp.com/api/'
+    app: 'https://agileprogramming.herokuapp.com/',
+    api: 'https://agileprogramming.herokuapp.com/api/'
   }
 };
